2ed21b19385d1ee257593cd9f0e889ec79e74de9
Érika Cristina Castro <erika.contax@gmail.com>
2025-11-21 09:43:50 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,86 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String phome;
+	private String passaword;
+
+	public User() {
+		
+	}
+
+	public User(Long id, String nome, String email, String phome, String passaword) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.phome = phome;
+		this.passaword = passaword;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhome() {
+		return phome;
+	}
+
+	public void setPhome(String phome) {
+		this.phome = phome;
+	}
+
+	public String getPassaword() {
+		return passaword;
+	}
+
+	public void setPassaword(String passaword) {
+		this.passaword = passaword;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
