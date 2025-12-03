8980179b6587ff13ee87f713a49b3ba7318a304f
Érika Cristina Castro <erika.contax@gmail.com>
2025-12-03 16:25:08 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -12,24 +12,34 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
-	private static final long serialVersionUID = 1L;
+@Table(name = "tb_product")
+
+public class Product implements Serializable{
+	
+	private static final long serialVersionUID = 1L; 
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	private Long id;
 	private String name;
-	private Set<Product> products = new HashSet<>();
+	private String description;
+	private double price;
+	private String imgUrl;
 
-	public Category() {
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
 
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -48,12 +58,33 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public String getDescription() {
+		return description;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -69,8 +100,9 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 
+	
 }
